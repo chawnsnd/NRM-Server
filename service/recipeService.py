0e6133d7c638ab6eff659ec04efa64ae977da72f
bkh-laptop-inferior <bkh751@gmail.com>
2018-11-19 14:46:08 +0900

    Id 로 가져오기 서비스 제거, recommendMenu 제거 , ( recommendRecipe 를 통쨰로 가져오는 것으로 메뉴 접근)

--- service/recipeService.py
+++ service/recipeService.py
@@ -6,54 +6,18 @@ from random import randint
 from dao.recipeDao import *
 
 #use recipeDao only
-def recommendMenu():
-    RecipesInDB = query_recipes_all()
-    print(RecipesInDB)
-    recommended = RecipesInDB[randint(0,len(RecipesInDB)-1)]
-    return recommended['menu']
-
 
 def recommendRecipe():
     RecipesInDB = query_recipes_all()
     recommendedRecipe = RecipesInDB[randint(0,len(RecipesInDB)-1)]
-    return recommendedRecipe['name']
-
-def nextStepById(recipeId,stepNo):
-    recipe = query_recipe_with_id(recipeId)
-    recipeSize = len(recipe)
-    stepNo+=1
-    if stepNo >=recipeSize:
-        return False , 0
-    step = recipe[stepNo]
-    return step , stepNo
-
-def previousStepById(recipeId,stepNo):
-    recipe = query_recipe_with_id(recipeId)
-    stepNo-=1
-    if stepNo < 0:
-        return False , 0
-    step = recipe[stepNo]
-    return step , stepNo
-
-def numberStepById(recipeId,stepNo):
-    recipe = query_recipe_with_id(recipeId)
-    if stepNo <0 or stepNo >= len(recipe):
-        return False , 0
-    step = recipe[stepNo]
-    return step , stepNo
-
-def getIngredientsById(recipeId):
-    ingredients = query_ingredients_with_id(recipeId)
-    if ingredients is None:
-        return False
-    return ingredients
+    return recommendedRecipe
 
 
 def numberStepByMenu(recipeMenu,stepNo):
     recipe = query_recipe_with_menu(recipeMenu)
     if stepNo <0 or stepNo >= len(recipe):
         return False , 0
-    step = recipe[stepNo]
+    step = recipe['steps'][stepNo]
     return step , stepNo
 
 def getIngredientsByMenu(recipeMenu):
