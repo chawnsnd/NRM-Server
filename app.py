4ebda1eb4ddd2de0a24d9ba4ca05195f305801f3
Junwoong <chawnsnd@gmail.com>
2018-11-22 13:09:56 +0900

    isNew

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def checkStepExist():
 #세션유지되는지?
 @app.before_request
 def before_request():
-    print(request.json)
+    print(request.json['context']['session']['isNew'])
 
 #0. 심사를 위한 health
 @app.route("/health", methods=["GET"])
